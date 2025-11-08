15e09b3e7d677fb461e44c1b4a62eea55a838e61
mitaliradia <mitaliradia2947@gmail.com>
2025-11-08 11:12:05 +0530

    css changed

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ function get_db() {
     $host = 'localhost';
     $dbname = 'mindful_moments';
     $username = 'root';
-    $password = 'pookie2102';
+    $password = '1234';
     
     $conn = new mysqli($host, $username, $password, $dbname);
     if ($conn->connect_error) {
